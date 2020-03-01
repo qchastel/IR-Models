6c0fa450ad8d1e0d490a70b98ba0799e2c4b4449
qchastel <49200797+qchastel@users.noreply.github.com>
2020-03-01 19:53:55 +0100

    Booleen update

--- BooleanModel.java
+++ BooleanModel.java
@@ -1,53 +1,125 @@
-import java.util.Comparator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.TreeSet;
 
+public class ModeleBooleen {
+	
+	public ArrayList<TreeSet<HashMap<Doc, Integer>>> and;
+	public ArrayList<TreeSet<HashMap<Doc, Integer>>> or;
+	public ArrayList<TreeSet<HashMap<Doc, Integer>>> not;
+	public TraitementTexte tT;
+	public Index i;
+	public IndexInversé iI;
+	
+	
+	public ModeleBooleen(TraitementTexte t, Index index,IndexInversé indexI) {
+		tT=t;
+		i=index;
+		iI=indexI;
+	}
+	
+	public ArrayList<Doc> resultat(String s1, String s2, String s3){
+		and=resultatReq(tT.traiteTexte(s1));
+		or=resultatReq(tT.traiteTexte(s2));
+		not=resultatReq(tT.traiteTexte(s3));
+		
+		for (TreeSet<HashMap<Doc, Integer>> treeSet : and) {
+			for (HashMap<Doc, Integer> hashMap : treeSet) {
+				System.out.println(hashMap.entrySet());
+					
+			
+			}
+		}
+		
+		
+		return null;
+	}
+	
+	public ArrayList<TreeSet<HashMap<Doc, Integer>>> resultatReq(ArrayList<String> al){
+		ArrayList<TreeSet<HashMap<Doc,Integer>>> listDoc=new ArrayList<>();
+		for (String string : al) {
+			listDoc.add(iI.listDoc(string));
+		}
+		
+		return listDoc;
+	}
+	
+	
+	/*
+	public String transformationEnReq(String expressionExacte,String mots,String auMoins,String pasCeMot) {
+		ArrayList<String> nvExpressionExacte = new ArrayList<>();
+		nvExpressionExacte = tT.traiteTexte(expressionExacte);
+		if(nvExpressionExacte!=null) {
+			
+			return expressionExacte;
+		}else {
+			
+			return mots+"^AND^"+auMoins+"^OR^"+auMoins+"^NOT^"+pasCeMot;
+		}
+	}
+	*/
+	/*public void requete(String arguments){
+		ArrayList<String> motsTraites=tT.traiterTexte(arguments);
+		ArrayList<String> docChemin = new ArrayList<>();
+		
+		for(String s : motsTraites) {
+			docChemin.addAll(a.get(s));
+		}
+		
+		HashMap<String,Integer> c = new HashMap<>();
+		for(int i = 0; i< docChemin.size();i++) {
+			c.put(docChemin.get(i), 0);
+			docChemin.remove(0);
+			for(int y=i;y<docChemin.size();y++) {
+				if(docChemin.get(i).equals(docChemin.get(y))) {
+					c.replace(docChemin.get(i), c.get(docChemin.get(i))+1);
+					docChemin.remove(y);
+				}
+			}
+		}
+		
+	}
+	
+	
+	public HashSet<String> recherche(String requete){
+	
+		HashMap<HashMap<String,String>,Integer> incidenceTermeDocument = new HashMap<>();
+		
+		String[] decoupage,decoupageAND,decoupageOR,decoupageNOT;
+		boolean booleenDecoupage,booleenDecoupageAND,booleenDecoupageOR,booleenDecoupageNOT;
+		decoupageNOT = requete.split("^NOT^");
+		if(decoupageNOT.length > 1)
+			booleenDecoupageNOT=true;
+		else
+			booleenDecoupageNOT=false;
+		decoupageOR = requete.split("^OR^");
+		if(decoupageOR.length > 1)
+			booleenDecoupageOR=true;
+		else
+			booleenDecoupageOR=false;
+		decoupageAND = requete.split("^AND^");
+		if(decoupageOR.length > 1)
+			booleenDecoupageAND=true;
+		else
+			booleenDecoupageAND=false;
+		
+		ArrayList<String> decoupageArray = new ArrayList<>();
+		
+		decoupage = requete.split("^AND^","^OR^","^NOT^");
+		for (int i = 0; i < decoupage.length; i++) {
+			decoupageArray.add(decoupage[i]);
+		}
+		
+		
+		
+	}
+	*/
 
-
-/**
- * A simple implementation of the boolean IR model.
- * 
- * @author Eric Hildebrand
- * 
- *         Part of the Information Retrieval 2012 Basic IR models project.
- */
-public class BooleanModel extends AbstractModel {
-    
-    public BooleanModel( Document[] documents ) {
-        super( documents );
-    }
-    
-    @Override
-    public SortedMap<Document,Double> getDocuments( String queryString, boolean stem ) {
-        
-        DisjunctiveNormalForm dnf = new DisjunctiveNormalForm( queryString, stem );
-        HashMap<Document,Double> map = new HashMap<Document,Double>( );
-        Comparator<Document> comparator = new ValueComparator( map );
-        SortedMap<Document,Double> result = new TreeMap<Document,Double>( comparator );
-        
-        // Initialize all documents with similarity of 0
-        // For each document, check if any conjunction of the query's dnf is satisfied
-        // If so, update the documents similarity to 1
-        for ( Document document : fDocuments ) {
-            map.put( document, 0.0 );
-            for ( Map<String,Boolean> conjunction : dnf ) {
-                boolean match = true;
-                for ( String term : conjunction.keySet( ) ) {
-                    if ( ( conjunction.get( term ) && document.getFrequency( term ) == 0 ) || ( !conjunction.get( term ) && document.getFrequency( term ) > 0 ) ) {
-                        match = false;
-                        break; // Leave term loop
-                    }
-                }
-                if ( match ) { // Conjunction satisfied
-                    map.put( document, 1.0 );
-                    break; // Leave conjunction loop
-                }
-            }
-        }
-        result.putAll( map );
-        return result;
-    }
-    
 }
